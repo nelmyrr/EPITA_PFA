2ef44a28f682b4785b952f6a08bb7818ac5a0287
Hermex <marwan.yousef@epita.fr>
2026-02-14 22:40:18 +0100

    Created a better way to test functions in the test_integration.c file

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -12,35 +12,43 @@ double aIntegral(double x);
 
 double f(double x)
 {
-    return sin(x);
+    //return sin(x);
     //return x*x*x;
-    //return aIntegral(x);
+    return aIntegral(x);
     //return exp(3 * pow(x, ln(2 * x)));
 }
 
-double aIntegral(double x) {
+double aIntegral(double x) { // ~ 0.514 between 0 and 1
     double x2 = x * x;
     double s = sqrt(x2 + 2.0);
     return atan(s) / ((1.0 + x2) * s);
 }
 
+int test(double (*f)(double), double from, double to, int precision)
+{
+    double right  =    rightMethod(f, from, to, precision);
+    double left   =     leftMethod(f, from, to, precision);
+    double middle =   middleMethod(f, from, to, precision);
+    double trap   = trapezesMethod(f, from, to, precision);
+
+
+    printf("Precision: %d\n\tThe Right Method shows:       %f\n", precision, right);
+    printf("\tThe Left Method shows:        %f\n", left);
+    printf("\tThe Middle Method shows:      %f\n", middle);
+    printf("\tThe Trapezoidal Method shows: %f\n", trap);
+
+    return 0;
+}
+
 int main()
 {
-
-    int precision = 1;
-    double from = -10;
-    double to   = 10;
-
-    double right  =    rightMethod(&f, from, to, precision);
-    double left   =     leftMethod(&f, from, to, precision);
-    double middle =   middleMethod(&f, from, to, precision);
-    double trap   = trapezesMethod(&f, from, to, precision);
-
-
-    printf("Precision: %d\n The Right Method shows:       %f\n", precision, right);
-    printf(" The Left Method shows:        %f\n", right);
-    printf(" The Middle Method shows:      %f\n", right);
-    printf(" The Trapezoidal Method shows: %f\n", right);
+    test(&f, 0, 1, 1);
+    test(&f, 0, 1, 5);
+    test(&f, 0, 1, 10);
+    test(&f, 0, 1, 50);
+    test(&f, 0, 1, 100);
+    test(&f, 0, 1, 500);
+    test(&f, 0, 1, 1000);
 
     return 0;
 }
