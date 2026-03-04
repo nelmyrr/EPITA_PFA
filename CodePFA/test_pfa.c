1b9781e47c1dae8ae25c8506c15e45a7ba8175df
hermex <mrwn.ysef@gmail.com>
2026-03-04 08:01:45 +0100

    Added good test functions in test_pfa.c | Modified test_pfa.c

--- CodePFA/test_pfa.c
+++ CodePFA/test_pfa.c
@@ -8,7 +8,7 @@
 /* Code here anything you want to test your pfa functions and to show your numerical results */
 int main()
 {
-    if (!init_integration("gauss3", 0.1)) {
+    if (!init_integration("gauss3", 0.001)) {
         printf("Failed to initialize integration.\n");
         return 1;
     }
@@ -16,7 +16,7 @@ int main()
     // ######### Put Your Tests Here #########
 
     Option option = {
-        PUT,
+        CALL,
         100.0,
         115.0,
         2.0,
@@ -24,27 +24,65 @@ int main()
         0.02
     };
 
-    printf("######### optionPrice #########\nS_0 = %.2f\nT = %.2f\nmu = %.2f\nsigma = %.2f\n", option.S0, option.T, option.mu, option.sig);
+    printf("######### Tests de la fonction optionPrice #########\n");
+    printf("S0 = 100 | T = 2 | mu = 0.05 | sigma = 0.02\n");
 
-    double Ks[10] = {115, 130, 155};
+    option.S0 = 100;
+    option.T = 2;
+    option.mu = 0.05;
+    option.sig = 0.02;
 
-    for (int i = 0; i < 3; i++)
+    double Ks[5] = {99, 100, 101, 102, 103};
+
+    for (int i = 0; i < 5; i++)
     {
-        option.K = *(Ks + i);
-        double price = optionPrice(&option);
-        printf("Put  price with K = %3.0f: %.5f\n", option.K, price);
+        option.K = *(Ks+i);
+        printf("K = %.0f | call price = %.5f\n", *(Ks+i), optionPrice(&option));
     }
 
-    option.type = CALL;
-    *(Ks+0) = 15;
-    *(Ks+1) = 50;
-    *(Ks+2) = 100;
+    printf("S0 = 100 | K = 101 | mu = 0.05 | sigma = 0.02\n");
+    option.S0 = 100;
+    option.K = 101;
+    option.mu = 0.05;
+    option.sig = 0.02;
+    double Ts[5] = {1.8, 1.9, 2, 2.1, 2.2};
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 5; i++)
+    {
+        option.T = *(Ts+i);
+        printf("T = %.1f | call price = %.5f\n", *(Ts+i), optionPrice(&option));
+    }
+
+
+    printf("S0 = 100 | T = 2 | mu = 0.05 | sigma = 0.02\n");
+    option.T = 2;
+    *(Ks+0) = 113;
+    *(Ks+1) = 114;
+    *(Ks+2) = 115;
+    *(Ks+3) = 116;
+    *(Ks+4) = 117;
+    option.type = PUT;
+
+    for (int i = 0; i < 5; i++)
     {
         option.K = *(Ks + i);
         double price = optionPrice(&option);
-        printf("Call price with K = %.0f: %.5f\n", option.K, price);
+        printf("K = %.0f | put price = %.5f\n", option.K, price);
+    }
+
+    printf("S0 = 100 | K = 115 | mu = 0.05 | sigma = 0.02\n");
+    option.K = 115;
+    *(Ts+0) = 1.8;
+    *(Ts+1) = 1.9;
+    *(Ts+2) = 2;
+    *(Ts+3) = 2.1;
+    *(Ts+4) = 2.2;
+
+    for (int i = 0; i < 5; i++)
+    {
+        option.T = *(Ts + i);
+        double price = optionPrice(&option);
+        printf("T = %.1f | put price = %.5f\n", option.K, price);
     }
 
 
@@ -52,6 +90,9 @@ int main()
     double phi2 = PHI(1);
     printf("######### PHI Testing #########\n\tPHI(-1) = %.4f\n\tPHI(1) = %.4f\n", phi1, phi2);
 */
+
+
+    printf("\n######### Insurance functions tests #########\n");
     double *array = malloc(sizeof(double) * 3);
     *array = 0.9;
     *(array+1) = 0.05;
@@ -71,18 +112,18 @@ int main()
  16     p_1 = p_2 = 0.05
 
 */
-    printf("\n######### Insurance Tests #########\nm = s = 1\np_0 = 0.9\np_1 = p_2 = 0.05\n");
+    printf("m = s = 1\np_0 = 0.9\np_1 = p_2 = 0.05\n");
 
-    double x = 1;
+    double x = 0;
 
-    for (x = 1; x < 11; x++)
+    for (x = 1; x < 4; x++)
     {
         double pdfX1X2 = clientPDF_X1X2(&client, x);
         double cdfX1X2 = clientCDF_X1X2(&client, x);
 
         double cdfS = clientCDF_S(&client, x);
 
-        printf("######### Results x = %.0f #########\n     PDF X1X2 = %.4f\n     CDF X1X2 = %.4f\n     CDF S    = %.4f\n", x, pdfX1X2, cdfX1X2, cdfS);
+        printf("x = %.0f\n     PDF X1X2 = %.8f\n     CDF X1X2 = %.8f\n     CDF S    = %.8f\n", x, pdfX1X2, cdfX1X2, cdfS);
     }
 
     free(array);
